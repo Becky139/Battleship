74bbbc372746f5d1cc78e9934120bd7ee55ffc08
Becky139 <rebecca.marriott@outlook.com>
2022-10-27 13:01:47 +0000

    change color of battleship

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def welcome_message():
     """
     print(
         """\
-    \u001b[32m
+    \u001b[35m
   ____        _   _   _           _     _           
  |  _ \      | | | | | |         | |   (_)          
  | |_) | __ _| |_| |_| | ___  ___| |__  _ _ __  ___ 
@@ -78,8 +78,8 @@ BOTH OF YOU CANT SEE WHERE TO"
     # Instructions - Markers
     print("MARKERS: \n")
     print("@ IS A SHIP")
-    print("\u001b[31m-\u001b[0m IS A MISS\n")
-    print("\u001b[32mX\u001b[0m IS A HIT/SUNK SHIP\n")
+    print("\u001b[31m-\u001b[0m IS A MISS")
+    print("\u001b[32mX\u001b[0m IS A HIT/SUNK SHIP")
     time.sleep(5)
     print(PHASE)
 
